75bd7ac114795104d932fee556bd937efe39d5c1
Geographer <geographer@geographer.fr>
2017-04-18 15:35:26 +0200

    Repair fzf fish integration

--- fish/config.fish
+++ fish/config.fish
@@ -15,10 +15,6 @@ set -g -x EDITOR vim
 # Default user
 set -x -g default_user geographer
 
-# Prompt colors
-# Seoul 256
-set dangerous_colors 3a3a3a 4e4e4e 626262 d0d0d0 d8af5f ffd787 d68787 d75f87 85add4 add4fb 5f865f 87af87
-
 # Colored man
 set -x -g LESS_TERMCAP_mb (printf "\033[01;31m")
 set -x -g LESS_TERMCAP_md (printf "\033[01;31m")
@@ -29,9 +25,7 @@ set -x -g LESS_TERMCAP_ue (printf "\033[0m")
 set -x -g LESS_TERMCAP_us (printf "\033[01;32m")
 
 # fzf
-set -x -g FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
-set -x -g FZF_CTRL_T_OPTS "--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
-set -x -g FZF_ALT_C_OPTS "--preview 'tree -C {} | head -200'"
+set -x -g FZF_ALT_C_OPTS "--preview 'tree {} | head -200'"
 
 # Private aliases
 source ~/.private-dotfiles/private_aliases
