4d5ef9e44bc7b2a14aecda0e02476bbf288e9fe6
Geographer <geographer@geographer.fr>
2017-10-22 18:10:05 +0200

    Cargo path

--- fish/config.fish
+++ fish/config.fish
@@ -27,6 +27,9 @@ set -xg LESS_TERMCAP_so (printf "\033[01;31;33m")
 set -xg LESS_TERMCAP_ue (printf "\033[0m")
 set -xg LESS_TERMCAP_us (printf "\033[01;32m")
 
+# Add Cargo to PATH
+set -gx PATH $HOME/.cargo/bin $PATH
+
 # Personal infos
 # Used in vim snippets
 set -xg FULLNAME "Lucas Santoni"
