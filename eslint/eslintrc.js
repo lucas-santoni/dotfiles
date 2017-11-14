4c40562a296b2a1829840c805138a96f002db928
Geographer <geographer@live.fr>
2017-11-15 00:18:26 +0100

    Indent

--- eslint/eslintrc.js
+++ eslint/eslintrc.js
@@ -1,40 +1,40 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-console": [
-            "error",
-            { allow: ["log", "warn", "error", "trace"] }
-        ],
-        "no-unused-vars": [
-            "error",
-            {
-              "varsIgnorePattern": "[iI]gnore",
-              "argsIgnorePattern": "[iI]gnore"
-            }
-        ]
-    }
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-console": [
+      "error",
+      { allow: ["log", "warn", "error", "trace"] }
+    ],
+    "no-unused-vars": [
+      "error",
+      {
+        "varsIgnorePattern": "[iI]gnore",
+        "argsIgnorePattern": "[iI]gnore"
+      }
+    ]
+  }
 };
