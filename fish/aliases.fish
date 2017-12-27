7b9d82a583dd188ae0f6fa1b5dcc9b371bcc25d3
Geographer <geographer@live.fr>
2017-12-27 16:07:35 +0100

    Add docker aliases
    Clean up useless aliases
    Change format, spaces instead of =

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,53 +1,68 @@
 ## VIM
-alias e="nvim"
-alias v="open -a Neovim.app"
+alias e "nvim"
 
 ## LS
-alias ls="exa"
-alias l="ls -l"
-alias la="ls -la"
+alias ls "exa"
+alias l "ls -l"
+alias la "ls -la"
 
 ## TREE
-alias tree="exa --tree"
-alias t="tree"
+alias tree "exa --tree"
+alias t "tree"
 
 ## GIT
-alias g="git"
-alias gd="git diff"
-alias gl="git pull"
-alias gst="git status"
-alias ga="git add"
-alias gaa="git add --all"
-alias gc="git commit"
-alias gp="git push"
+alias g "git"
+alias gd "g diff"
+alias gl "g pull"
+alias gst "g status"
+alias ga "g add"
+alias gaa "g add --all"
+alias gc "g commit"
+alias gp "g push"
 
 ## RM
-alias rrm="rm -Rf"
+alias rrm "rm -Rf"
 
 ## CP
-alias cp="cp -v"
-alias ccp="cp -R"
+alias cp "cp -v"
+alias ccp "cp -R"
 
 ## MV
-alias mv="mv -v"
+alias mv "mv -v"
 
 ## TMUX
-alias tmux="tmux -2"
-alias tm="tmux"
-alias at="tmux attach -t"
-alias tmls="tmux list-sessions"
+alias tmux "tmux -2"
+alias tm "tmux"
+alias at "tm attach -t"
+alias tmls "tm list-sessions"
 
 ## MAKE
-alias m="make"
-alias mr="make re"
+alias m "make"
+alias mr "m re"
 
 ## PYTHON
-alias python="python3"
-alias p="python"
-alias ip="ipython3"
+alias python "python3"
+alias p "python"
+alias ip "ipython3"
 
 ## CHMOD
-alias cx="chmod +x"
+alias cx "chmod +x"
 
 ## GDB
-alias gdb="gdb -q"
+alias gdb "gdb -q"
+
+## DOCKER
+alias d "docker"
+alias drm "d rm"
+alias da "d start"
+alias do "d stop"
+alias di "d images"
+alias dps "d ps"
+alias dpsa "dps -a"
+
+## DOCKER-COMPOSE
+alias dc "docker-compose"
+alias dcu "dc up"
+alias dcd "dc down"
+alias dcps "dc ps"
+alias dpsa "dcps -a"
