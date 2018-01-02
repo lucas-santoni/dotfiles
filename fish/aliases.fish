1b22bdd62ea914e0bde784628c31e40036299a8f
Geographer <geographer@live.fr>
2018-01-02 16:20:45 +0100

    Fix aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,24 +1,22 @@
 ## VIM
 alias e "nvim"
 
-## LS
+## LS/TREE/EXA
 alias ls "exa"
-alias l "ls -l"
-alias la "ls -la"
-
-## TREE
+alias l "exa -l"
+alias la "exa -la"
 alias tree "exa --tree"
-alias t "tree"
+alias t "exa --tree"
 
 ## GIT
 alias g "git"
-alias gd "g diff"
-alias gl "g pull"
-alias gst "g status"
-alias ga "g add"
-alias gaa "g add --all"
-alias gc "g commit"
-alias gp "g push"
+alias gd "git diff"
+alias gl "git pull"
+alias gst "git status"
+alias ga "git add"
+alias gaa "git add --all"
+alias gc "git commit"
+alias gp "git push"
 
 ## RM
 alias rrm "rm -Rf"
@@ -32,17 +30,17 @@ alias mv "mv -v"
 
 ## TMUX
 alias tmux "tmux -2"
-alias tm "tmux"
-alias at "tm attach -t"
-alias tmls "tm list-sessions"
+alias tm "tmux -2"
+alias at "tmux attach -t"
+alias tmls "tmux list-sessions"
 
 ## MAKE
 alias m "make"
-alias mr "m re"
+alias mr "make re"
 
 ## PYTHON
 alias python "python3"
-alias p "python"
+alias p "python3"
 alias ip "ipython3"
 
 ## CHMOD
@@ -53,16 +51,19 @@ alias gdb "gdb -q"
 
 ## DOCKER
 alias d "docker"
-alias drm "d rm"
-alias da "d start"
-alias do "d stop"
-alias di "d images"
-alias dps "d ps"
-alias dpsa "dps -a"
+alias drm "docker rm"
+alias da "docker start"
+alias do "docker stop"
+alias di "docker images"
+alias dps "docker ps"
+alias dpsa "docker ps -a"
 
 ## DOCKER-COMPOSE
 alias dc "docker-compose"
-alias dcu "dc up"
-alias dcd "dc down"
-alias dcps "dc ps"
-alias dpsa "dcps -a"
+alias dcu "docker-compose up"
+alias dcd "docker-compose down"
+alias dcps "docker-compose ps"
+alias dcpsa "docker-compose ps -a"
+
+## BLIH
+alias b "blih"
