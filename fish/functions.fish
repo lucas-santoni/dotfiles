51f687356a6e141b6f4bcce0ea7347cad3f7663a
Geographer <geographer@live.fr>
2018-01-04 14:39:43 +0100

    Add blih functions

--- fish/functions.fish
+++ fish/functions.fish
@@ -7,3 +7,21 @@ function macOS
   vmware-hgfsclient
   vmhgfs-fuse -o allow_other -o auto_unmount .host:/geographer /home/geographer/macOS
 end
+
+function crepo
+  blih repository create $argv[1]
+  blih repository setacl $argv[1] ramassage-tek r
+  echo (blih repository getacl $argv[1])
+end
+
+function drepo
+  blih repository delete $argv[1]
+end
+
+function grepo
+  blih repository getacl $argv[1]
+end
+
+function srepo
+  blih repository setacl $argv[1] $argv[2] $argv[3]
+end
