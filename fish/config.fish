8a7bc3fc27a534fe70e4e32746ebc74c2e1fe407
Geographer <geographer@live.fr>
2018-03-27 01:15:20 +0200

    Custom bindings

--- fish/config.fish
+++ fish/config.fish
@@ -16,13 +16,33 @@ set fish_color_valid_path
 function fish_vi_cursor; end
 fish_vi_key_bindings
 
-# Still enable ctrl+f in Vi mode
+# Bindings !
 function fish_user_key_bindings
+  # Still enable ctrl+f in Vim mode
   for mode in insert default visual
     bind -M $mode \cf forward-char
   end
 
+  # Get the default FZF functions and bindings
   fzf_key_bindings
+
+  # Set the custom bindings
+  bind -e \cr
+  bind \ch fzf-history-widget
+  bind -e \ct
+  bind \cj fzf-file-widget
+  bind -e \ec
+  bind \ck fzf-cd-widget
+
+  # Vim mode compatibility
+  if bind -M insert > /dev/null 2>&1
+    bind -M insert -e \cr
+    bind -M insert \ch fzf-history-widget
+    bind -M insert -e \ct
+    bind -M insert \cj fzf-file-widget
+    bind -M insert -e \ec
+    bind -M insert \ck fzf-cd-widget
+  end
 end
 
 # FZF configuration
