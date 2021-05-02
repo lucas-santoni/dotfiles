bd83e6a73454d7dc4988f0467eb31e85d6acd135
Geographer <geographer@live.fr>
2021-05-02 17:53:41 +0200

    Emacs configuration and update of the rest

---
+++ emacs/init.el
@@ -0,0 +1,10 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'org)
+(org-babel-load-file (expand-file-name "settings.org" user-emacs-directory))
