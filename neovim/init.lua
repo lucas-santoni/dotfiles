20e7790b5fcde4e3bfd12b6ca5c7164c1a7d9992
Geographer <geographer@live.fr>
2021-07-12 00:41:58 +0200

    Sync

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,3 +1,32 @@
+-- Colors
+local background =     '#181818'
+local background_l =   '#282828'
+local background_ll =  '#383838'
+local background_lll = '#585858'
+local foreground_d =   '#b8b8b8'
+local foreground =     '#d8d8d8'
+local foreground_l =   '#e8e8e8'
+local white =          '#f8f8f8'
+local red =            '#ab4642'
+local orange =         '#dc9656'
+local yellow =         '#f7ca88'
+local green =          '#a1b56c'
+local teal =           '#86c1b9'
+local blue =           '#7cafc2'
+local pink =           '#ba8baf'
+local brown =          '#a16946'
+
+-- Map utility
+function map(arr, f)
+  local res = {}
+
+  for k, v in pairs(arr) do
+    res[k] = f(v)
+  end
+
+  return res
+end
+
 -- Variables for quick access to 'built-in' stuff
 local cmd = vim.cmd
 local fn = vim.fn
@@ -13,48 +42,74 @@ local packer = require('packer')
 -- Packer is inspired by use-package so package configuration can be defined
 -- here too but I prefer to have the package configurations outside for now
 packer.startup(function()
-  use { 'wbthomason/packer.nvim', opt = true }
-  use 'jiangmiao/auto-pairs'
-  use 'folke/which-key.nvim'
-  use 'hrsh7th/nvim-compe'
-  use 'tpope/vim-commentary'
-  use 'famiu/nvim-reload'
-  use 'RRethy/nvim-base16'
-  use 'kyazdani42/nvim-web-devicons'
-  use {
+  use({ 'wbthomason/packer.nvim', opt = true })
+  use('jiangmiao/auto-pairs')
+  use('folke/which-key.nvim')
+  use('tpope/vim-commentary')
+  use('norcalli/nvim-base16.lua')
+  use('famiu/nvim-reload')
+  use({
     'nvim-telescope/telescope.nvim',
     requires = {{ 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' }}
-  }
+  })
+  use('neovim/nvim-lspconfig')
+  use('hrsh7th/nvim-compe')
+  use('folke/trouble.nvim')
+  use('onsails/lspkind-nvim')
+  use('lukas-reineke/indent-blankline.nvim')
+  use('glepnir/galaxyline.nvim')
 end)
 
 -- Variables for quick packages access
-local telescope = require('telescope')
-local reload = require('nvim-reload')
-local compe = require('compe')
-local wk = require('which-key')
+local base16      = require('base16')
+local telescope   = require('telescope')
+local tbuiltin    = require('telescope.builtin')
+local reload      = require('nvim-reload')
+local compe       = require('compe')
+local wk          = require('which-key')
+local lsp         = require('lspconfig')
+local compe       = require('compe')
+local trouble     = require('trouble')
+local lspkind     = require('lspkind')
+local galaxy      = require('galaxyline')
 
 -- Setup colorscheme
 -- We could probably use a Lua function exposed by the package here but who
 -- cares and that's what the documentation recommends anyways
-cmd('set termguicolors')
-cmd('colorscheme base16-default-dark')
+local enable_256_color_space = true
+local theme_array = {
+  background   ; background_l ; background_ll ; background_lll ;
+  foreground_d ; foreground   ; foreground_l  ; white ;
+  red          ; orange       ; yellow        ; green ;
+  teal         ; blue         ; pink          ; brown ;
+}
+local theme_array_clean = map(theme_array, function(x) return x:sub(2) end)
+local theme = base16.theme_from_array(theme_array_clean)
+base16(theme, enable_256_color_space)
 
 -- Various Vim settings
-opt.expandtab = true                  -- Use spaces instead of tabs
-opt.hidden = true                     -- Enable background buffers
-opt.ignorecase = true                 -- Ignore case
-opt.scrolloff = 4                     -- Lines of context
-opt.shiftround = true                 -- Round indent
-opt.shiftwidth = 2                    -- Size of an indent
-opt.sidescrolloff = 8                 -- Columns of context
-opt.smartcase = true                  -- Do not ignore case with capitals
-opt.smartindent = true                -- Insert indents automatically
-opt.splitbelow = true                 -- Put new windows below current
-opt.splitright = true                 -- Put new windows right of current
-opt.tabstop = 2                       -- Number of spaces tabs count for
-opt.termguicolors = true              -- True color support
-opt.wrap = false                      -- Disable line wrap
--- opt.signcolumn = 'yes'                -- Disable line wrap
+-- I guess just use :help <something> when in doubt
+opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.shiftround = true
+opt.hidden = true
+opt.showcmd = false
+opt.ignorecase = true
+opt.scrolloff = 4
+opt.sidescrolloff = 8
+opt.smartcase = true
+opt.smartindent = true
+opt.splitbelow = true
+opt.splitright = true
+opt.termguicolors = true
+opt.wrap = false
+opt.signcolumn = 'yes'
+opt.number = false
+opt.cursorline = true
+opt.completeopt = 'menuone,noselect'
+opt.pumheight = 10
 
 -- Telescope settings
 telescope.setup({
@@ -84,68 +139,266 @@ end
 
 -- Change the character used as a vertical split
 -- No clue how to do this in Lua
-vim.cmd('set fillchars+=vert:\\|')
+vim.cmd('set fillchars+=vert:\\▏')
 
--- Make the vertical split less visible
-hi('VertSplit', { guibg = 'NONE', guifg='#ff0000', ctermbg='NONE', ctermfg=6 })
--- hi('SignColumn', { guibg = 'NONE' })
--- hi('StatusLine', { guibg = '#cccccc' })
--- hi('StatusLineNC', { guibg = '#cccccc', guifg = '#bbbbbb' })
+hi('VertSplit', { guibg = 'NONE' })
+hi('SignColumn', { guibg = 'NONE' })
+
+-- Avoid unthemed character
+-- https://github.com/vim-airline/vim-airline-themes/issues/48
+hi('StatusLine', { guibg = background_l })
+hi('StatusLineNC', { guibg = background_l })
+
+hi('LineNR', { guibg = 'NONE', guifg = background_ll })
+hi('CursorLine', { guibg = 'NONE', guifg = 'NONE' })
+hi('CursorLineNR', { guibg = 'NONE', guifg = background_lll })
+
+hi('LspDiagnosticsDefaultError', { guibg = 'NONE', guifg = red })
+hi('LspDiagnosticsDefaultWarning', { guibg = 'NONE', guifg = orange })
+hi('LspDiagnosticsDefaultInformation', { guibg = 'NONE', guifg = blue })
+hi('LspDiagnosticsDefaultHint', { guibg = 'NONE', guifg = blue })
+
+hi('TroubleCount', { guibg = background_lll, guifg = background })
+hi('TroubleFoldIcon', { guibg = 'NONE', guifg = background_ll })
+hi('TroubleIndent', { guibg = 'NONE', guifg = background_ll })
+hi('TroubleLocation', { guibg = 'NONE', guifg = background_lll })
+hi('TroubleSource', { guibg = 'NONE', guifg = background_lll })
+
+hi('IndentBlanklineChar', { guibg = 'NONE', guifg = background_l })
+
+hi('TelescopeSelection', { guibg = 'NONE', guifg = foreground })
+hi('TelescopeNormal', { guibg = background, guifg = foreground })
+
+hi('TelescopeBorder', { guifg = background_lll })
+hi('TelescopePromptBorder', { guifg = background_lll })
+hi('TelescopeResultsBorder', { guifg = background_lll })
+hi('TelescopePreviewBorder', { guifg = background_lll })
+
+hi('TelescopeMatching', { guifg = red })
+
+hi('TelescopePromptPrefix', { guifg = red })
 
 -- Configure netrw
 g.netrw_banner = 0
 
--- To be converted
-vim.cmd('set completeopt=menuone,noselect')
-vim.cmd('set shortmess+=c')
-
 -- Remap the default leader (;) to ' ' (space)
 g.mapleader = ' '
 
+-- Configuration for which-key
 wk.setup({
   icons = {
-    breadcrumb = "»",
-    separator = "➜ ",
-    group = "+",
+    breadcrumb = '»',
+    separator = '➜ ',
+    group = '+',
   },
   window = {
-    border = "single",
-    position = "bottom",
+    border = 'none',
+    position = 'bottom',
     margin = { 1, 0, 1, 0 },
-    padding = { 0, 0, 0, 0 },
+    padding = { 1, 0, 1, 0 },
   },
   layout = {
     height = { min = 4, max = 25 },
     width = { min = 20, max = 50 },
     spacing = 3,
-    align = "left",
+    align = 'left',
   },
   show_help = true,
 })
 
+-- Mappings for which-key
 wk.register({
-  ["<leader>"] = { "<cmd>Telescope buffers<cr>", "Buffer List" },
-  ["n"] = { "<cmd>nohl<cr>", "No Highlight" },
+  ['<leader>'] = { tbuiltin.buffers, 'Buffer List' },
+  ['n'] = { '<cmd>nohl<cr>', 'No Highlight' },
   a = {
-    name = "windows/buffer",
-    h = { "<C-w>h", "Window Left" },
-    j = { "<C-w>j", "Window Down" },
-    k = { "<C-w>k", "Window Right" },
-    l = { "<C-w>l", "Window Up" },
-    n = { "<cmd>bnext<cr>", "Next Buffer" },
-    p = { "<cmd>bprev<cr>", "Previous Buffer" },
-    s = { "<cmd>split<cr>", "Split Horizontally" },
-    v = { "<cmd>vsplit<cr>", "Split Vertically" },
-    q = { "<cmd>close<cr>", "Close" },
+    name = 'windows/buffer',
+    o = { '<C-o>', 'Back Buffer' },
+    i = { '<C-i>', 'Forward Buffer' },
+    h = { '<C-w>h', 'Window Left' },
+    j = { '<C-w>j', 'Window Down' },
+    k = { '<C-w>k', 'Window Right' },
+    l = { '<C-w>l', 'Window Up' },
+    n = { '<cmd>bnext<cr>', 'Next Buffer' },
+    p = { '<cmd>bprev<cr>', 'Previous Buffer' },
+    d = { '<cmd>bdelete<cr>', 'Delete Buffer' },
+    s = { '<cmd>split<cr>', 'Split Horizontally' },
+    v = { '<cmd>vsplit<cr>', 'Split Vertically' },
+    c = { '<cmd>close<cr>', 'Close' },
+    q = { '<cmd>quit<cr>', 'Quit' },
+  },
+  d = {
+    name = 'document',
+    s = { '<cmd>w<cr>', 'Save Changes' },
+    S = { '<cmd>wa<cr>', 'Save All Changes' },
+    r = { '<cmd>u0<cr>', 'Restore Changes' },
   },
   f = {
-    name = "find",
-    b = { "<cmd>Telescope buffers<cr>", "Find Buffers" },
-    f = { "<cmd>Telescope find_files<cr>", "Find Files" },
-    g = { "<cmd>Telescope live_grep<cr>", "Find Text" },
+    name = 'find',
+    b = { tbuiltin.buffers, 'Find Buffers' },
+    c = { tbuiltin.command_history, 'Command History' },
+    C = { tbuiltin.commands, 'Command' },
+    f = { tbuiltin.git_files, 'Find Files (Git)' },
+    F = { tbuiltin.find_files, 'Find Files (All)' },
+    g = { tbuiltin.live_grep, 'Find Text' },
+    o = { tbuiltin.vim_options, 'Vim Option' },
+    p = { tbuiltin.oldfiles, 'Previously Opened' },
+    r = { tbuiltin.registers, 'Registers' },
+  },
+  l = {
+    name = 'lsp',
+    t = { '<cmd>TroubleToggle lsp_document_diagnostics<cr>', 'Document Diagnostics' },
+    T = { '<cmd>TroubleToggle lsp_workspace_diagnostics<cr>', 'Workspace Diagnostics' },
+    r = { tbuiltin.lsp_references, 'References' },
+    d = { tbuiltin.lsp_definitions, 'Definitions' },
+    i = { tbuiltin.lsp_implementations, 'Implementations' },
   },
   m = {
-    name = "misc",
-    s = { "<cmd>source %<cr>", "Source current" },
+    name = 'misc',
+    s = { '<cmd>source %<cr>', 'Source current' },
   },
-}, { prefix = "<leader>" })
+  p = {
+    name = 'packer',
+    s = { '<cmd>PackerSync<cr>', 'Packer Sync' },
+    u = { '<cmd>PackerUpdate<cr>', 'Packer Update' },
+  },
+}, { prefix = '<leader>' })
+
+
+-- Enabled LSP servers
+lsp.gopls.setup({})
+
+-- Configuration for Compe
+compe.setup({
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'disable';
+  documentation = false;
+  source = {
+    path = true;
+    buffer = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+    vsnip = false;
+    calc = false;
+    ultisnips = false;
+    luasnip = false;
+  };
+})
+
+-- Configuration for trouble
+trouble.setup({
+  icons = false,
+  fold_open = '',
+  fold_closed = '',
+  indent_lines = true,
+  auto_open = false,
+  auto_close = false,
+  auto_preview = false,
+  auto_fold = false,
+  signs = {
+    error = 'error',
+    warning = 'warn',
+    hint = 'hint',
+    information = 'info',
+    other = 'other',
+  },
+})
+
+-- LSP icons
+local signs = { Error = '▸ ', Warning = '▸ ', Hint = '▸ ', Information = '▸ ' }
+for type, icon in pairs(signs) do
+  local hl = 'LspDiagnosticsSign' .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+end
+
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
+vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = false,
+  underline = false,
+}
+)
+
+lspkind.init({
+  symbol_map = {
+    Text = ' ',
+    Method = 'ƒ ',
+    Function = 'ƒ ',
+    Constructor = 'ƒ ',
+    Variable = ' ',
+    Class = ' ',
+    Interface = ' ',
+    Module = ' ',
+    Property = ' ',
+    Unit = ' ',
+    Keyword = ' ',
+    Snippet = '﬌ ',
+    Color = ' ',
+    File = ' ',
+    Folder = ' ',
+    Enum = ' ',
+    EnumMember = ' ',
+    Value = ' ',
+    Constant = ' ',
+    Struct = ' '
+  },
+})
+
+g.indentLine_enabled = 1
+g.indent_blankline_char = '▏'
+g.indent_blankline_filetype_exclude = {
+  'help',
+  'terminal',
+  'dashboard'
+}
+g.indent_blankline_buftype_exclude = {
+  'terminal'
+}
+g.indent_blankline_show_trailing_blankline_indent = false
+g.indent_blankline_show_first_indent_level = false
+
+local gls = galaxy.section
+galaxy.short_line_list = {'LuaTree','vista','dbui'}
+
+local colors = {
+  bg = '#282c34',
+  yellow = '#fabd2f',
+  cyan = '#008080',
+  darkblue = '#081633',
+  green = '#afd700',
+  orange = '#FF8800',
+  purple = '#5d4d7a',
+  magenta = '#d16d9e',
+  grey = '#c0c0c0',
+  blue = '#0087d7',
+  red = '#ec5f67'
+}
+
+local buffer_not_empty = function()
+  if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then
+    return true
+  end
+  return false
+end
+
+function line()
+  local bg = background_l
+
+  gls.left[1] = {
+    FileName = {
+      provider = { 'FileName' },
+      condition = buffer_not_empty,
+      highlight = { foreground, bg }
+    }
+  }
+
+  gls.right[1] = {
+    PerCent = {
+      provider = 'LinePercent',
+      highlight = {foreground, bg},
+    }
+  }
+end
+
+line()
